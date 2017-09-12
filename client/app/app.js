659191abea7041a60d2c2ebd608e4a940fbba257
ParvBhardwaj <bhparv@gmail.com>
2017-09-12 16:54:25 +0530

    .

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('movieMelaApp', ['movieMelaApp.auth', 'movieMelaApp.admin', 'movieMelaApp.constants',
-    'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'btford.socket-io', 'ui.bootstrap',
-    'validation.match'
-  ])
-  .config(function($routeProvider, $locationProvider) {
+  'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'btford.socket-io', 'ui.bootstrap',
+  'validation.match'
+])
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider.otherwise({
       redirectTo: '/'
     });
